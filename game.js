04cea1930c13084325f4843264a72512d0efee6a
Tony Wong <t.wong0902@gmail.com>
2022-10-15 02:05:41 -0400

    mmmm

--- game.js
+++ game.js
@@ -3,18 +3,25 @@ let ctx = canvas.getContext("2d");
 // unit_resolution is the sqrt(number of pixels) in a single cell
 let unit_resolution = 200;
 // Uses unit_resolution to get full canvas size
-let canvas_width = 32*unit_resolution;
-let canvas_height = 33*unit_resolution;
+let canvas_width = 65*unit_resolution;
+let canvas_height = 37*unit_resolution;
+// How the grid actually looks on the page
+let canvas_style_width = 1900;
+let canvas_style_height = 945;
 // Sets attributes through js so I can use variables, tbh don't know how to do it otherwise
-canvas.setAttribute("width",canvas_width)
-canvas.setAttribute("height",canvas_height)
-canvas.setAttribute("style","width:500px; height:500px;")
+canvas.width = canvas_width;
+canvas.height = canvas_height;
+canvas.style.width = canvas_style_width.toString()+"px";
+canvas.style.height = canvas_style_height.toString()+"px";
 // Fill with green for now
 ctx.fillStyle = "rgb(0,255,0)";
 // Create canvas
 ctx.fillRect(0,0,canvas_width,canvas_height)
 // Set how thicc the line drawn is, change this to use math later
 ctx.lineWidth = 10;
+window.scrollTo((canvas_style_width-screen.width)/2,(canvas_style_height-screen.height)/2);
+
+
 // Drawing the veritcal grid lines
 for (let i = 0; i <= canvas_width/unit_resolution; i++) {
     ctx.strokeStyle = "Black";
@@ -34,49 +41,43 @@ for (let i = 0; i <= canvas_height/unit_resolution; i++) {
     ctx.stroke();
 }
 
-// Draws the sprite for griffin with parameters (x, y) with x and y being the top-left coordinates of image
-function make_griffin(x, y) {
-    griffin = new Image();
-    griffin.src = "sprites/griffin_final.png";
-    griffin.onload = function(){
-        ctx.drawImage(griffin, x, y, unit_resolution, unit_resolution);
+// Creating griffin sprite
+let griffin = document.getElementById("griffin");
+griffin.width = unit_resolution*(canvas_style_width/canvas_width)
+griffin.height = unit_resolution*(canvas_style_height/canvas_height)
+griffin.style.marginLeft = ((canvas_style_width/2)-unit_resolution*(canvas_style_width/canvas_width)/2).toString()+"px";
+griffin.style.marginTop = ((canvas_style_height/2)-unit_resolution*(canvas_style_height/canvas_height)/2).toString()+"px";
+
+// Creating steak sprite
+let steak = document.getElementById("steak");
+steak.width = unit_resolution*(canvas_style_width/canvas_width);
+steak.height = unit_resolution*(canvas_style_height/canvas_height);
+
+window.addEventListener("keydown", getKeyPress);
+
+window.addEventListener("keydown", (e) => {if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.code)) {e.preventDefault();}}, false);
+
+function getKeyPress(event) {
+    var scaled_pixel_width = (unit_resolution*(canvas_style_width/canvas_width));
+    var scaled_pixel_height = (unit_resolution*(canvas_style_height/canvas_height));
+    switch (event.code){
+        case 'KeyA': case 'ArrowLeft':
+            griffin.style.marginLeft = (parseFloat(griffin.style.marginLeft)-scaled_pixel_width).toString()+"px";
+            window.scrollTo(window.scrollX-scaled_pixel_width, window.scrollY);
+            break;
+        case 'KeyS': case 'ArrowDown':
+            griffin.style.marginTop = (parseFloat(griffin.style.marginTop)+scaled_pixel_height).toString()+"px";
+            window.scrollTo(window.scrollX, window.scrollY+scaled_pixel_height);
+            break;
+        case 'KeyD': case 'ArrowRight':
+            griffin.style.marginLeft = (parseFloat(griffin.style.marginLeft)+scaled_pixel_width).toString()+"px";
+            window.scrollTo(window.scrollX+scaled_pixel_width, window.scrollY);
+            break;
+        case 'KeyW': case 'ArrowUp':
+            griffin.style.marginTop = (parseFloat(griffin.style.marginTop)-scaled_pixel_height).toString()+"px";
+            window.scrollTo(window.scrollX, window.scrollY-scaled_pixel_height);
+            break;
     }
+
 }
 
-// Draws the sprite for steak with parameters (x, y) with x and y being the top-left coordinates of image
-function make_steak(x,y) {
-    steak = new Image();
-    steak.src = "sprites/steak_devil_final.png";
-    steak.onload = function(){
-        ctx.drawImage(steak, x, y, unit_resolution, unit_resolution);
-    }
-}
-
-make_steak(0,0);
-make_griffin(unit_resolution,unit_resolution)
-
-
-
-
-// Below is boiler-plate code for future use
-// Game loop
-
-function update(progress) {
-    // Update the state of the world for the elapsed time since last render
-}
-
-function draw() {
-    // Draw the state of the world
-}
-
-function loop(timestamp) {
-    var progress = timestamp - lastRender
-
-    update(progress)
-    draw()
-
-    lastRender = timestamp
-    window.requestAnimationFrame(loop)
-}
-var lastRender = 0
-window.requestAnimationFrame(loop)
